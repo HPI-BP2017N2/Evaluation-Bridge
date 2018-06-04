c887606c0d1023e6528d8bd42436b96ece42e9a9
deeps96 <leonardo.huebscher.business@gmail.com>
2018-06-04 21:01:55 +0200

    Fix persisting sample offers.

--- src/main/java/de/hpi/evaluationbridge/dto/IdealoOffer.java
+++ src/main/java/de/hpi/evaluationbridge/dto/IdealoOffer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import org.jsoup.nodes.Document;
+import org.springframework.data.annotation.Transient;
 
 import java.util.List;
 import java.util.Map;
@@ -13,6 +14,7 @@ import java.util.Map;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class IdealoOffer {
 
+    @Transient
     private Document fetchedPage;
 
     private Property<String> ean, sku, han, brandName;
--- src/main/java/de/hpi/evaluationbridge/dto/IdealoOffers.java
+++ src/main/java/de/hpi/evaluationbridge/dto/IdealoOffers.java
@@ -1,10 +1,6 @@
 package de.hpi.evaluationbridge.dto;
 
-import de.hpi.evaluationbridge.service.OfferAttribute;
-
 import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class IdealoOffers extends LinkedList<IdealoOffer> {
 
